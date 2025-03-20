2bf2a318e95ab66c27f209e6bc45c261f2ced84d
Ben Willmore <ben@willmore.eu>
2025-03-20 10:07:47 +0000

    Build script for i386

--- build-i386.sh
+++ build-i386.sh
@@ -3,11 +3,12 @@ BUILD_DATE=`date +"%Y%m%d_%H%M%S"`
 GIT_HASH=`git rev-parse HEAD`
 GIT_BRANCH=`git branch --show-current`
 VERSION="${1:-TESTING}"
-hwplatform=`uname -i`
+hwplatform=i386
 [[ "${hwplatform}" == "aarch64" ]] && PATHLIB="/usr/lib/aarch64-linux-gnu" && ARCH="aarch64"
 [[ "${hwplatform}" == "armv7l" ]] && PATHLIB="/usr/lib/arm-linux-gnueabihf" && ARCH="armhf"
+[[ "${hwplatform}" == "i386" ]] && PATHLIB="/usr/lib/i386-linux-gnu" && ARCH="i386"
 
-gcc hacksdl.c configuration.c debug.c  "${PATHLIB}/libconfig.a" -o "hacksdl.${ARCH}.so" -fPIC -shared -lSDL2 -D_GNU_SOURCE -Wl,--defsym,BUILD_DATE_${BUILD_DATE}=0 -Wl,--defsym,GIT_HASH_${GIT_HASH}=0 -Wl,--defsym,GIT_BRANCH_${GIT_BRANCH}=0 -Wl,--defsym,VERSION_${VERSION}=0
+gcc hacksdl.c configuration.c debug.c -m32 "${PATHLIB}/libconfig.a" -o "hacksdl.${ARCH}.so" -m32 -fPIC -shared -lSDL2 -D_GNU_SOURCE -Wl,--defsym,BUILD_DATE_${BUILD_DATE}=0 -Wl,--defsym,GIT_HASH_${GIT_HASH}=0 -Wl,--defsym,GIT_BRANCH_${GIT_BRANCH}=0 -Wl,--defsym,VERSION_${VERSION}=0
 
 strip "hacksdl.${ARCH}.so"
 cp "hacksdl.${ARCH}.so" "hacksdl.so"
